2b7792f521b5cecf6f3fac350054769d7d949e62
huangwx8 <1017417793@qq.com>
2022-01-29 14:21:19 +0800

    lab thread: 实现barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,7 +25,19 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
+  // BEGIN LAB THREAD
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if (bstate.nthread == nthread) { // every thread reaches the barrier
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    bstate.nthread = 0;
+    bstate.round++;
+  }
+  else
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  pthread_mutex_unlock(&bstate.barrier_mutex);
+  // END LAB THREAD
+
   //
   // Block until all threads have called barrier() and
   // then increment bstate.round.
