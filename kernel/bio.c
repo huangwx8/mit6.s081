52b25fb307fd5e641cf2eac87cc0e3d2bc020798
huangwx8 <1017417793@qq.com>
2022-01-31 10:50:33 +0800

    lab lock: 减小fs.bio层的锁粒度，提高并发度，部分情况下存在哈希冲突

--- kernel/bio.c
+++ kernel/bio.c
@@ -31,7 +31,56 @@ struct {
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
   struct buf head;
+
+#define HASH_TABLE_SIZE 65537
+#define HASH_FUNC(dev, blockno) (((dev + 7) * (blockno + 11) + 57) % HASH_TABLE_SIZE)
+  int key2bidx[HASH_TABLE_SIZE];
+  int bidx2key[NBUF];
+  uint timestamp[NBUF];
+  
+  struct spinlock buflocks[NBUF];
 } bcache;
+
+void addkey(uint64 key, int bindex) {
+  bcache.key2bidx[key] = bindex;
+  bcache.bidx2key[bindex] = key;
+}
+
+void delkey(int bindex) {
+  uint64 key = bcache.bidx2key[bindex];
+  if (key != -1) {
+    bcache.key2bidx[key] = -1;
+    bcache.bidx2key[bindex] = -1;
+  }
+}
+
+uint64 findevictee() {
+  uint eticks = 0xffffffff;
+  uint64 eindex = -1;
+  struct buf* b;
+
+  for (uint64 i = 0; i < NBUF; i++) {
+    acquire(&bcache.buflocks[i]);
+    b = &bcache.buf[i];
+    if (b->refcnt != 0) {
+      release(&bcache.buflocks[i]);
+      continue;
+    }
+    if (bcache.timestamp[i] <= eticks) {
+      // release old evictee's lock
+      if (eindex != -1)
+        release(&bcache.buflocks[eindex]);
+      // update
+      eticks = bcache.timestamp[i];
+      eindex = i;
+      // hold new evictee's lock
+      continue;
+    }
+    release(&bcache.buflocks[i]);
+  }
+
+  return eindex;
+}
 
 void
 binit(void)
@@ -50,6 +99,14 @@ binit(void)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
+
+  for (int i = 0; i < HASH_TABLE_SIZE; i++)
+    bcache.key2bidx[i] = -1;
+  for (int i = 0; i < NBUF; i++) {
+    bcache.bidx2key[i] = -1;
+    bcache.timestamp[i] = 0;
+    initlock(&bcache.buflocks[i], "bcache");
+  }
 }
 
 // Look through buffer cache for block on device dev.
@@ -58,23 +115,37 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
+  //printf("bget dev = %d, blockno = %d\n", dev, blockno);
   struct buf *b;
 
-  acquire(&bcache.lock);
+  //acquire(&bcache.lock);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  /*for(b = bcache.head.next; b != &bcache.head; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
       release(&bcache.lock);
       acquiresleep(&b->lock);
       return b;
     }
+  }*/
+  uint64 key, bindex;
+  key = HASH_FUNC(dev, blockno);
+  if ((bindex = bcache.key2bidx[key]) != -1) {
+    acquire(&bcache.buflocks[bindex]);
+    b = &bcache.buf[bindex];
+    if (b->dev != dev || b->blockno != blockno)
+      panic("hash conflict");
+    b->refcnt++;
+    release(&bcache.buflocks[bindex]);
+    //release(&bcache.lock);
+    acquiresleep(&b->lock);
+    return b;
   }
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
+  /*for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
     if(b->refcnt == 0) {
       b->dev = dev;
       b->blockno = blockno;
@@ -84,7 +155,23 @@ bget(uint dev, uint blockno)
       acquiresleep(&b->lock);
       return b;
     }
+  }*/
+
+  bindex = findevictee(); // could implictly acquire evictee's lock here
+  if (bindex != -1) {
+    delkey(bindex);
+    addkey(key, bindex);
+    b = &bcache.buf[bindex];
+    b->dev = dev;
+    b->blockno = blockno;
+    b->valid = 0;
+    b->refcnt = 1;
+    release(&bcache.buflocks[bindex]);
+    //release(&bcache.lock);
+    acquiresleep(&b->lock);
+    return b;
   }
+  
   panic("bget: no buffers");
 }
 
@@ -116,24 +203,33 @@ bwrite(struct buf *b)
 void
 brelse(struct buf *b)
 {
+  //printf("brelse dev = %d, blockno = %d\n", b->dev, b->blockno);
   if(!holdingsleep(&b->lock))
     panic("brelse");
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  //acquire(&bcache.lock);
+
+  uint64 key, bindex;
+  key = HASH_FUNC(b->dev, b->blockno);
+  bindex = bcache.key2bidx[key];
+
+  acquire(&bcache.buflocks[bindex]);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
-    b->next->prev = b->prev;
+    /*b->next->prev = b->prev;
     b->prev->next = b->next;
     b->next = bcache.head.next;
     b->prev = &bcache.head;
     bcache.head.next->prev = b;
-    bcache.head.next = b;
+    bcache.head.next = b;*/
+    bcache.timestamp[bindex] = ticks;
   }
-  
-  release(&bcache.lock);
+  release(&bcache.buflocks[bindex]);
+
+  //release(&bcache.lock);
 }
 
 void
